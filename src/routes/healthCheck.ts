4c6ed19ad309e2ebaeddd773fde2c9892a299bca
Stephen McGuckin <donald.stephen.mcguckin@gmail.com>
2019-09-03 01:26:18 -0700

    added json to response

--- src/routes/healthCheck.ts
+++ src/routes/healthCheck.ts
@@ -2,7 +2,7 @@ import {Router} from 'express';
 
 export default (app: Router) => {
     app.get('/healthCheck', (req: any, res: any, next: any) => {
-        res.json();
+        res.json({health: 'ok'});
     });
 };
 
