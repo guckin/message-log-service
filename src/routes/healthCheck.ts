18e2286ff1950db498963c519e2b7e9db040ece5
Stephen McGuckin <donald.stephen.mcguckin@gmail.com>
2019-09-03 02:06:57 -0700

    Server sends 200 instead of 304

--- src/routes/healthCheck.ts
+++ src/routes/healthCheck.ts
@@ -2,7 +2,8 @@ import {Router} from 'express';
 
 export default (app: Router) => {
     app.get('/healthCheck', (req: any, res: any, next: any) => {
-        res.json({health: 'ok'});
+        console.log('hey');
+        res.status(200).send({healthCheck: 'OK'});
     });
 };
 
