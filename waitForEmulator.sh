500b2c06d40ebe312e61e1c4c840d3c257d1fc16
Stephen McGuckin <donald.stephen.mcguckin@gmail.com>
2019-09-09 01:19:46 -0700

    makes the integration test run setup a manual process
    Dont want to run everything in one script in case the tests fail, if
    this happens the docker-compose would never happen

--- waitForEmulator.sh
+++ waitForEmulator.sh
@@ -1,9 +1,6 @@
-#!/usr/bin/env bash -e
-docker-compose up > /dev/null 2>&1 &
+#!/usr/bin/env bash
 NEXT_WAIT_TIME=0
 until curl -X GET http://localhost:8081/  || [[ ${NEXT_WAIT_TIME} -eq 300 ]]; do
    [[ NEXT_WAIT_TIME++ ]]
    sleep 1
 done
-jest --config ./jest.integration.json
-docker-compose down
